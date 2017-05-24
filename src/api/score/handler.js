e82e56e175c7b9a766548c4820745e49eddfb506
Nick Schulze <nicks@dwolla.com>
2017-05-24 15:33:04 -0500

    refactor: add joi to package.json

--- src/api/score/handler.js
+++ src/api/score/handler.js
@@ -6,4 +6,5 @@ exports.userScore = (request, reply) => {
   const createdDate = new Date()
   const score = Math.round(createdDate.getMilliseconds() / 10)
   reply({ id: Uuid(), createdDate, score }).code(200)
+
 }
