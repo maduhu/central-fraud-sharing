1601351f37acaa2a5c0ce745cdf161a8edaa5e74
Nick Schulze <nicks@dwolla.com>
2017-05-24 17:03:10 -0500

    feat: return user score based on ms

--- src/api/score/handler.js
+++ src/api/score/handler.js
@@ -6,5 +6,4 @@ exports.userScore = (request, reply) => {
   const createdDate = new Date()
   const score = Math.round(createdDate.getMilliseconds() / 10)
   reply({ id: Uuid(), createdDate, score }).code(200)
-
 }
