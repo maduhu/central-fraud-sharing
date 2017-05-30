b25c5bd82c1fd70d0b3962cf4ca5a5f03c1538a8
Nick Schulze <nicks@dwolla.com>
2017-05-30 11:52:31 -0500

    refactor: remove unnecessary config values

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,7 +3,5 @@ const RC = require('rc')('FSHARE', require('../../config/default.json'))
 module.exports = {
   HOSTNAME: RC.HOSTNAME.replace(/\/$/, ''),
   DATABASE_URI: RC.DATABASE_URI,
-  PORT: RC.PORT,
-  END_USER_REGISTRY_URL: RC.END_USER_REGISTRY_URL,
-  SCHEME_ID: RC.SCHEME_ID
+  PORT: RC.PORT
 }
