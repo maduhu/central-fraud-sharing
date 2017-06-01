52c44e9f79f6844d2e9d8b09683d1f2f612d1555
Nick Schulze <nicks@dwolla.com>
2017-06-01 14:32:02 -0500

    fix: deploy on circle ci

--- deploy/node/variables.js
+++ deploy/node/variables.js
@@ -15,7 +15,7 @@ module.exports = {
   DOCKER_EMAIL: process.env.DOCKER_EMAIL,
   DOCKER_USER: process.env.DOCKER_USER,
   DOCKER_PASS: process.env.DOCKER_PASS,
-  HOSTNAME: process.env.HOSTNAME || 'http://central-fraud-sharing-TEST-2067903239.us-west-2.elb.amazonaws.com',
+  HOSTNAME: process.env.HOSTNAME || 'http://central-fraud-sharing-2067903239.us-west-2.elb.amazonaws.com',
   JFROG_REPO: process.env.JFROG_REPO || 'modusbox-level1-docker-release.jfrog.io',
   POSTGRES_USER: process.env.DEV_POSTGRES_USER,
   POSTGRES_PASSWORD: process.env.DEV_POSTGRES_PASSWORD,
